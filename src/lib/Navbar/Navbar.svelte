694f136f057cd7857633c43378ceafe16d9e0b84
novaotp <rahmansajidur30@gmail.com>
2024-01-22 23:06:26 +0100

    chore: formatted with prettier

--- src/lib/Navbar/Navbar.svelte
+++ src/lib/Navbar/Navbar.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-    import Link from "./Link.svelte";
-    import { IconHome as Home, IconArticle as Blog, } from "@tabler/icons-svelte";
+	import Link from './Link.svelte';
+	import { IconHome as Home, IconArticle as Blog } from '@tabler/icons-svelte';
 </script>
 
 <!--
@@ -9,11 +9,13 @@
 Renders a navbar adapted for devices under 1024px wide.
 -->
 
-<nav class="lg:hidden relative w-full h-20 shadow-[0px_-4px_3px_rgba(50,50,50,0.25)] dark:shadow-[0px_-4px_3px_rgba(10,10,10,0.25)]">
-    <ul class="relative w-full h-full py-5 flex justify-evenly items-center">
-        <Link href="/" label="Home" />
-        <Link href="/work" label="Projects" />
-        <Link href="/blog" label="Blog" />
-        <Link href="/contact" label="Contact" />
-    </ul>
+<nav
+	class="lg:hidden relative w-full h-20 shadow-[0px_-4px_3px_rgba(50,50,50,0.25)] dark:shadow-[0px_-4px_3px_rgba(10,10,10,0.25)]"
+>
+	<ul class="relative w-full h-full py-5 flex justify-evenly items-center">
+		<Link href="/" label="Home" />
+		<Link href="/work" label="Projects" />
+		<Link href="/blog" label="Blog" />
+		<Link href="/contact" label="Contact" />
+	</ul>
 </nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script>
-    import "../app.css";
-    import Navbar from "$lib/Navbar/Navbar.svelte";
+	import '../app.css';
+	import Navbar from '$lib/Navbar/Navbar.svelte';
 </script>
 
 <div class="relative w-full h-full flex flex-col">
-    <main class="relative w-full flex-grow">
-        <slot />
-    </main>
-    <Navbar />
+	<main class="relative w-full flex-grow">
+		<slot />
+	</main>
+	<Navbar />
 </div>
