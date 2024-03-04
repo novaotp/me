19639737abecd70e6d0c48975b790e79d05d286b
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-03-04 10:54:53 +0100

    feat: adapting styles for accessibility

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,37 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-
-	onMount(() => {
-		document.title = "Home - Sajidur Rahman";
-	})
+	import { IconPointFilled } from '@tabler/icons-svelte';
 </script>
 
-<div class="relative w-full h-full flex justify-center items-center flex-col">
-	<h1 class="text-center font-bold text-3xl mb-10">Bonjour, je suis Sajidur Rahman.</h1>
-	<p class="text-center">[Insérez un texte sur ma vie ici]</p>
+<svelte:head>
+	<title>Home - Sajidur Rahman</title>
+	<meta
+		name="description"
+		content="Découvrez le portfolio de Sajidur Rahman, développeur web axé sur les interfaces utilisateur pour un design fluide et une satisfaction
+	client optimale. Explorez les projets uniques que j'ai créés et contactez-moi pour une collaboration."
+	/>
+</svelte:head>
+
+<div class="relative w-full h-full flex justify-center items-start flex-col gap-10">
+	<h1 class="">Bonjour, je suis Sajidur Rahman.</h1>
+	<p class="text-justify text-3xl">
+		Un développeur web axé sur les interfaces utilisateur pour un design fluide et une satisfaction
+		client optimale.
+	</p>
+	<ul class="relative flex flex-wrap">
+		<li>Design</li>
+		<li><IconPointFilled /></li>
+		<li>Développement</li>
+		<li><IconPointFilled /></li>
+		<li>Maintenance</li>
+	</ul>
+	<a href="/contact">
+		<span
+			class="relative py-3 px-10 flex justify-center items-center bg-indigo-700 text-white rounded-t-lg"
+			>Un projet en tête ?</span
+		>
+		<span
+			class="relative py-3 px-10 flex justify-center items-center border-2 border-indigo-700 rounded-b-lg"
+			>Parlons-en</span
+		>
+	</a>
 </div>
