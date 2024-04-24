d7d3675584f6ee1761293f515f781d05c0662069
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-04-24 18:46:31 +0200

    fix: toast notifications color mismatch would cause them to be invisible

--- src/lib/components/shared/Toast/Toast.svelte
+++ src/lib/components/shared/Toast/Toast.svelte
@@ -12,7 +12,7 @@
     export let type: 'success' | 'error' | 'info';
 
     $: icon = type === 'success' ? SuccessIcon : type === 'error' ? ErrorIcon : InfoIcon;
-    $: bgColor = type === 'success' ? 'bg-accent-success-600' : type === 'error' ? 'bg-accent-danger-500' : 'bg-primary-500';
+    $: bgColor = type === 'success' ? 'bg-green-600' : type === 'error' ? 'bg-red-500' : 'bg-blue-500';
 </script>
 
 <article class="relative w-full flex items-center gap-4 rounded px-4 py-2 text-neutral-50 {bgColor}" role="alert" transition:fade>
