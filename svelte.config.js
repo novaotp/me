647481a22951a33bfd02b9ce878a87a2ffbb5ab2
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-04-24 21:30:38 +0200

    fix: added correct adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from 'svelte-adapter-static-digitalocean';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            precompress: false
+        }),
 		alias: {
 			$stores: './src/lib/stores',
 			$i18n: './src/i18n'
