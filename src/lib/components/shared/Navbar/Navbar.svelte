7a62061b06faa9a394ca90e910006557b833cf9a
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-04-24 20:19:52 +0200

    fix: fixed wrong links on desktop

--- src/lib/components/shared/Navbar/Navbar.svelte
+++ src/lib/components/shared/Navbar/Navbar.svelte
@@ -3,6 +3,8 @@
 	import IconMenu from "@tabler/icons-svelte/IconMenu.svelte";
 	
 	import { page } from '$app/stores';
+    import { locale } from "$i18n/i18n-svelte";
+    import { base } from "$app/paths";
 
 	export let show: boolean;
 
@@ -29,11 +31,11 @@ Renders a navbar adapted for devices under 1024px wide.
 </nav>
 
 <nav class="lg:flex hidden w-full h-20 px-10 py-5 justify-between items-center">
-	<a href="/" class="py-1 border-b-2 hover:border-black {pathname === "/" ? "border-black" : "border-transparent"}">Sajidur Rahman</a>
+	<a href="{base}/{$locale}" class="py-1 border-b-2 hover:border-black {pathname === "/" ? "border-black" : "border-transparent"}">Sajidur Rahman</a>
 	<div class="flex items-center gap-12">
-		<a href="/work" class="py-1 border-b-2 hover:border-black {pathname === "/work" ? "border-black" : "border-transparent"}">Mes travaux</a>
-		<a href="/blog" class="py-1 border-b-2 hover:border-black {pathname === "/blog" ? "border-black" : "border-transparent"}">Blog</a>
-		<a href="/contact" class="bg-indigo-700 py-2 px-6 rounded-lg text-[#fff6ea] flex gap-5 items-center">
+		<a href="{base}/{$locale}/work" class="py-1 border-b-2 hover:border-black {pathname === "/work" ? "border-black" : "border-transparent"}">Mes travaux</a>
+		<a href="{base}/{$locale}/blog" class="py-1 border-b-2 hover:border-black {pathname === "/blog" ? "border-black" : "border-transparent"}">Blog</a>
+		<a href="{base}/{$locale}/contact" class="bg-indigo-700 py-2 px-6 rounded-lg text-[#fff6ea] flex gap-5 items-center">
 			Travaillons ensemble <IconArrowRight />
 		</a>
 	</div>
