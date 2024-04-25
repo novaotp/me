fc876fd5de70e021f2bbbe48523bc7cf88404993
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-04-25 06:53:55 +0200

    fix: removed static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,11 @@
-import adapter from 'svelte-adapter-static-digitalocean';
-// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            precompress: false
-        }),
-		// adapter(),
+		adapter: adapter(),
 		alias: {
 			$stores: './src/lib/stores',
 			$i18n: './src/i18n'
