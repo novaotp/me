58b8512220a8fa5ddf8836c99cb610f0263d0033
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-04-25 06:20:40 +0200

    fix: debugging why / is not redirected in prod v3

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,12 @@
+import { base } from '$app/paths';
+import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = ({ locals: { locale } }) => {
+export const load: LayoutServerLoad = ({ locals: { locale }, url }) => {
+	if (url.pathname === "/") {
+		throw redirect(307, `${base}/${locale}`)
+	}
+
 	return { locale };
 }
 
--- src/routes/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { base } from "$app/paths";
-import { redirect } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
-
-export const load: PageServerLoad = async ({ locals: { locale } }) => {
-    throw redirect(307, `${base}/${locale}`)
-};
