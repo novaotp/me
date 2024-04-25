7c50967f48c2e69dfc14c9344a5f412b946d7aa4
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-04-25 07:12:16 +0200

    fix: testing with prerendered pages on vercel

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,3 +3,6 @@ import type { LayoutServerLoad } from './$types';
 export const load: LayoutServerLoad = ({ locals: { locale } }) => {
 	return { locale };
 }
+
+export const prerender = true;
+export const ssr = false;
