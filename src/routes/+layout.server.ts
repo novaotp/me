4dc3a5d2cd0c7461836477b16fd3913a653a4348
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-04-25 06:51:39 +0200

    fix: removing prerendering for only ssr

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,6 +3,3 @@ import type { LayoutServerLoad } from './$types';
 export const load: LayoutServerLoad = ({ locals: { locale } }) => {
 	return { locale };
 }
-
-export const prerender = true;
-export const ssr = false;
