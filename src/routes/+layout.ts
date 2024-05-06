4fae6bae2d78aecf3f740d93c62b6ef9a826d1d8
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-05-06 22:59:09 +0200

    perf: added vercel analytics for monitoring

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,7 +3,11 @@ import type { Locales } from '../i18n/i18n-types'
 import { loadLocaleAsync } from '../i18n/i18n-util.async'
 import { i18nObject } from '../i18n/i18n-util.js'
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+ 
+// Vercel analytics + speed insights
+inject({ mode: dev ? 'development' : 'production' });
 injectSpeedInsights();
 
 export const load: LayoutLoad<{ locale: Locales }> = async ({ data: { locale } }) => {
