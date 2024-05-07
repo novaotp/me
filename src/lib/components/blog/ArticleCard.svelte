e8265b9aa52a4d86f326c18a331dcff1241d8f76
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-05-07 18:38:07 +0200

    fix: fixed stretched img on article card

--- src/lib/components/blog/ArticleCard.svelte
+++ src/lib/components/blog/ArticleCard.svelte
@@ -13,7 +13,9 @@
     on:click={() => goto(`${base}/${$locale}/blog/${filename}`, { noScroll: false })}
     class="relative rounded-lg shadow-[0_0_15px_0px_rgba(0,0,0,0.25)] duration-150 hover:scale-105 hover:shadow-[0_0_25px_0px_rgba(0,0,0,0.25)] flex flex-col items-start overflow-hidden {$$restProps.class ?? ''}"
 >
-    <img src="{banner}" alt="{bannerAlt}" class="relative w-full max-h-[200px] bg-cover bg-center" />
+    <div class="relative w-full h-[200px] flex justify-center items-center overflow-hidden">
+        <img src="{banner}" alt="{bannerAlt}" class="w-full bg-center bg-cover bg-no-repeat" />
+    </div>
     <div class="relative w-full flex flex-col items-start gap-[10px] p-10">
         <h1 class="relative w-full text-start text-xl">{shortTitle ?? title}</h1>
         <time class="text-gray-500 text-sm">{creationDate.toLocaleDateString($locale)}</time>
