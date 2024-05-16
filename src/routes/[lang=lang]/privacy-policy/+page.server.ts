7a788349e65e3876d53501fad671b03b75ecaba1
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-05-16 18:25:30 +0200

    fix(ci): added entries for privacy-policy page to allow prerendering

--- src/routes/[lang=lang]/privacy-policy/+page.server.ts
+++ src/routes/[lang=lang]/privacy-policy/+page.server.ts
@@ -1,9 +1,13 @@
 import type { Locales } from "$/i18n/i18n-types";
 import { privacyPolicy } from "$/lib/server/privacy-policy";
-import type { PageServerLoad } from "./$types";
+import type { EntryGenerator, PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params }) => {
     const content = await privacyPolicy(params.lang as Locales);
 
     return { ...content };
+};
+
+export const entries: EntryGenerator = () => {
+    return [{ lang: 'fr' }, { lang: 'en' }];
 };
