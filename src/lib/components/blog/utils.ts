305f16224612a8d27780088fe74c1035512caf9b
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-06-01 12:01:56 +0200

    fix: fixed issue with french tags not recognized

--- src/lib/components/blog/utils.ts
+++ src/lib/components/blog/utils.ts
@@ -1,12 +1,13 @@
 export const mapTagToColor = (tag: string) => {
     switch (true) {
-        case tag === "personal":
+        case ["personal", "personnel"].includes(tag):
             return "bg-green-600";
         case tag === "guide":
             return "bg-yellow-600"
         case ["sveltekit", "express", "socket.io"].includes(tag):
             return "bg-blue-500";
         default:
-            throw new Error("Unexpected tag received !");
+            console.warn(`Unsupported tag received : ${tag}, default color used.`);
+            return "bg-gray-500";
     }
 }
