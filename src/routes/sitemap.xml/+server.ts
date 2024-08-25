46ee0dbecc2e80c8a2885af6b911fb7f5d501a60
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-08-25 04:42:34 +0200

    fix: completed unfinished sitemap

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,7 +1,18 @@
-import { glob } from "glob";
+import { allCategories, importArticles } from "$lib/server/article";
+import { baseLocale } from "$i18n/i18n-util";
 
-export async function GET() {
-    console.log(await glob("./.svelte-kit/"))
+export async function GET({ url }) {
+    const paths = [
+        "/",
+        "/work",
+        "contact",
+        "/privacy-policy",
+        "/blog",
+        (await allCategories()).map(category => `/blog/${category}`),
+        // The locale itself doesn't matter.
+        (await importArticles(baseLocale)).map(article => `/blog/${article.metadata.category}/${article.filename}`)
+    ].flat();
+
     return new Response(
         `
             <?xml version="1.0" encoding="UTF-8" ?>
@@ -13,7 +24,13 @@ export async function GET() {
                 xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
                 xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
             >
-                <!-- <url> elements go here -->
+                ${paths.map(path => {
+                    return `
+                        <url>
+                            <loc>${url.hostname}${path}</loc>
+                        </url>
+                    `
+                })}
             </urlset>
         `.trim(),
         { headers: { 'Content-Type': 'application/xml' } }
