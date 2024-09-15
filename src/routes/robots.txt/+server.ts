5e7d94e8fbcd08d28458f79dbd8cac6ce15ad63d
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-09-16 00:30:06 +0200

    fix: fixed robots.txt's sitemap reference by preprending the hostname

--- src/routes/robots.txt/+server.ts
+++ src/routes/robots.txt/+server.ts
@@ -1,17 +1,5 @@
-import { base } from '$app/paths';
+export function GET({ url }) {
+    const content = ['User-agent: *', 'Disallow: ', `Sitemap: ${url.hostname}/sitemap.xml`].join('\n');
 
-export function GET() {
-    return new Response(
-        `
-            User-agent: *
-            Disallow: 
-
-            Sitemap: ${base}/sitemap.xml
-        `
-            .trim()
-            .split('\n')
-            .map((line) => line.trim())
-            .join('\n'),
-        { headers: { 'Content-Type': 'text/plain' } }
-    );
+    return new Response(content, { headers: { 'Content-Type': 'text/plain' } });
 }
