14e010508d0ae4a830881d65c1389dfaae2d1923
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-10-01 11:07:07 +0200

    fix: added protocol to sitemap's url in robots.txt

--- src/routes/robots.txt/+server.ts
+++ src/routes/robots.txt/+server.ts
@@ -1,5 +1,7 @@
-export function GET({ url }) {
-    const content = ['User-agent: *', 'Disallow: ', `Sitemap: ${url.hostname}/sitemap.xml`].join('\n');
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = ({ url }) => {
+    const content = ['User-agent: *', 'Disallow: ', `Sitemap: ${url.protocol}//${url.hostname}/sitemap.xml`].join('\n');
 
     return new Response(content, { headers: { 'Content-Type': 'text/plain' } });
-}
+};
