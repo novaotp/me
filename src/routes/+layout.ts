818690aa51f3f5dd6b0ca564f49ea89d95e63ea8
Sajidur Rahman <94069791+novaotp@users.noreply.github.com>
2026-08-18 05:11:21 +0200

    chore: removed vercel stuff

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,9 +1,1 @@
-import { dev } from '$app/environment';
-import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-import { inject } from '@vercel/analytics';
-
 export const prerender = true;
-
-// Vercel analytics + speed insights
-inject({ mode: dev ? 'development' : 'production' });
-injectSpeedInsights();
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
+import tailwindcss from '@tailwindcss/vite';
+import adapter from '@sveltejs/adapter-node';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    optimizeDeps: {
-        exclude: [
-            '@tabler/icons-svelte',
-            'typesafe-i18n',
-            'typesafe-i18n/detectors',
-            'typesafe-i18n/utils',
-            '@vercel/speed-insights/sveltekit',
-            '@vercel/analytics',
-            'typesafe-i18n/svelte',
-            'tailwind-merge',
-            'clsx'
-        ]
-    },
-    server: {
-        host: '127.0.0.1'
-    }
+    plugins: [
+        tailwindcss(),
+        sveltekit({
+            adapter: adapter(),
+            alias: {
+                $components: './src/lib/components',
+                $stores: './src/lib/stores'
+            },
+            compilerOptions: {
+                runes: ({ filename }) => {
+                    // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+                    return filename.split(/[/\\]/).includes('node_modules') ? undefined : true;
+                }
+            }
+        })
+    ]
 });
